f486e6a43c35becf796b32d5ec2efde2f602eb06
zhangrujun <3260700251@qq.com>
2024-09-17 03:07:27 +0800

    quickstart

---
+++ quickstart/src/main/java/top/flabby/quickstart/HelloServlet.java
@@ -0,0 +1,23 @@
+package top.flabby.quickstart;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>"+"2305271411  张如珺"+"</h1>");
+        out.println("</body></html>");
+    }
+}
